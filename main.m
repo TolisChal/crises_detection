26f79a5d05729c01013a25310bfbf9cb0609ecc2
TolisChal <tolis.chal@gmail.com>
2021-01-21 16:26:10 +0200

    update

--- main.m
+++ main.m
@@ -1,13 +1,17 @@
 close all; clc; clear all;
 
-
-addpath('clustering'); addpath('compute_emd'); addpath('copula_compuration');
+addpath('clustering'); addpath('compute_emd'); addpath('copula_computation');
 addpath('indicators'); addpath('plots'); addpath('reconstruction');
 addpath('small_copulas');
 
-returns = read_data();
+%T = readtable('asset_returns_daily_30_industry.csv');
+%AssRets = table2array(T) / 100;
+load('AssReturns_daily_30_industry.mat')
     
-copulas = compute_copulas(returns, 100, 60, 500000);
+copulas = compute_copulas(AssRets, 100, 60, 500000);
+save('copulas_30_industry.mat', 'copulas')
 
 indicators = comp_indicators_mask(copulas);
 ind_lbl = indicators_lbl(indicators);
+save('indicators_30_industry.mat', 'indicators')
+save('ind_lbl_30_industry.mat', 'ind_lbl')
